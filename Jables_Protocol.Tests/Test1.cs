470ee9a6b597886324c53548b62c89f8abc06f54
b-arkell <broarkell2@gmail.com>
2026-02-12 17:18:08 -0500

    added Protocol Testing Space
    Added Protocol testing space and included the protocol so it can be used in testing.

---
+++ Jables_Protocol.Tests/Test1.cs
@@ -0,0 +1,17 @@
+﻿using Jables_Protocol;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+
+namespace Jables_Protocol.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
