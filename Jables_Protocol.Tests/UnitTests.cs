2f9c6e7dd7068ed7c4027420b8062cad88b9ac7e
b-arkell <broarkell2@gmail.com>
2026-02-12 17:24:13 -0500

    Added Game Logic Tests Space
    Added Game Logic Test Space and separated the space into integration and unit tests. Also included Game Logic library so it can be used in testing later.

---
+++ Jables_Protocol.Tests/UnitTests.cs
@@ -0,0 +1,16 @@
+using Jables_Protocol;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+
+namespace Jables_Protocol.Tests;
+
+[TestClass]
+public class UnitTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
