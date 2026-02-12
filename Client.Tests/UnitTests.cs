bf2c0aa29f152c34e4bdd2d1a260662eaae17370
b-arkell <broarkell2@gmail.com>
2026-02-12 17:30:24 -0500

    Added Client Testing Space
    Added Client Testing space and relevant includes

---
+++ Client.Tests/UnitTests.cs
@@ -0,0 +1,16 @@
+using Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+
+namespace Client.Tests;
+
+[TestClass]
+public class UnitTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
