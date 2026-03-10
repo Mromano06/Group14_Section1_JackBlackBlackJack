8303ffdf70704f82748a69ea2a34b3e6f6c2062e
b-arkell <broarkell2@gmail.com>
2026-03-10 16:47:20 -0400

    Implemented the basic functionality of server network layer
    created a networkServer class, ClientConnection, and GameManager to allow communication between clients and server and can update the actual game

---
+++ Server/GameControl/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Policy;
+using System.Text;
+using GameLogic.Core;
+
+namespace Server.GameControl
+{
+    public class GameManager
+    {
+        // create game
+        private Game game = new Game();
+
+        public void HandleCommand(string command)
+        {
+            ///TODO: Create the logic that will manage and change the game based on commands
+        }
+
+    }
+}
---
+++ Server/Networking/NetworkServer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace Server.Networking
+{
+    public class NetworkServer
+    {
+        private TcpListener _listener;
+
+        private List<ClientConnection> clients = new List<ClientConnection>();
+
+        public async Task Start(int port)
+        {
+            _listener = new TcpListener(IPAddress.Any, port);
+            _listener.Start();
+
+            Console.WriteLine("Server listening...");
+        
+            while (true)
+            {
+                TcpClient tcpClient = await _listener.AcceptTcpClientAsync();
+
+                ClientConnection client = new ClientConnection(tcpClient);
+
+                clients.Add(client);
+
+                client.Start();
+
+                Console.WriteLine("Client Connected");
+                
+            }
+        
+        }
+    }
+}
