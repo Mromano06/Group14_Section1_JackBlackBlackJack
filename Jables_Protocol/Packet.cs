19b459d0b096fc8dbd76e3d35fb70672b2c7dc24
SAM PELOT <sampelot93@gmail.com>
2026-03-10 21:35:27 -0400

    Added To and FromBytes
    Added the to and from bytes to serialize the actual packet.

--- Jables_Protocol/Packet.cs
+++ Jables_Protocol/Packet.cs
@@ -3,6 +3,7 @@ using System.Numerics;
 using SharedModels.Models;
 using SharedModels.Core;
 using GameLogic.Core;
+using System.Security.Cryptography.X509Certificates;
 
 // Sam Pelot
 // WIP
@@ -10,16 +11,42 @@ using GameLogic.Core;
 
 namespace Jables_Protocol
 {
-    
+
     public class Packet
     {
-        public PacketType Type {  get; set; }
+        public PacketType Type { get; set; }
         public int PayloadSize { get; set; }
-        
+
         // initiallizes with an static empty cached array instance to avoid null references & multiple empty array instances
         public byte[] Payload { get; set; } = Array.Empty<byte>();
 
+
+        // To Bytes
+        public byte[] ToBytes()
+        {
+            using var ms = new MemoryStream();
+            using var bw = new BinaryWriter(ms);
+
+            bw.Write((byte)Type);
+            bw.Write(PayloadSize);
+            bw.Write(Payload);
+
+            return ms.ToArray();
+        }
+
+        // From Bytes
+        public static Packet FromBytes(byte[] data)
+        {
+            using var ms = new MemoryStream(data);
+            using var br = new BinaryReader(ms);
+
+            PacketType type = (PacketType)br.ReadByte();
+            int payloadSize = br.ReadInt32();
+            byte[] payload = br.ReadBytes(payloadSize);
+
+            return new Packet { Type = type, PayloadSize = payloadSize, Payload = payload };
+        }
+
     }
 
-    
 }
