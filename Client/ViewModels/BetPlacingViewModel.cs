2dbd5afbfc950eec417233294bd9481b56b9e2eb
Mromano06 <matbat2205@gmail.com>
2026-03-12 13:14:15 -0400

    Bet  Placing window implemented and working

--- Client/ViewModels/BetPlacingViewModel.cs
+++ Client/ViewModels/BetPlacingViewModel.cs
@@ -2,6 +2,7 @@
 using Client.Networking;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows.Input;
 
@@ -10,39 +11,63 @@ namespace Client.ViewModels
     public class BetPlacingViewModel : BaseModel
     {
         private readonly NetworkClient _client;
+        private readonly int _playerMoney;
+        private int _currentBet;
 
         public ICommand IncreaseBetCommand { get; }
         public ICommand DecreaseBetCommand { get; }
         public ICommand MaxBetCommand { get; }
-
         public ICommand ConfirmBetCommand { get; }
 
-        public BetPlacingViewModel(NetworkClient client)
+        public BetPlacingViewModel(NetworkClient client, int playerMoney)
         {
-            this._client = client;
-
+            // TODO: Remove the default 100 money for testing
+            _client = client;
+            // this._playerMoney = playerMoney;
+            _playerMoney = 100;
             IncreaseBetCommand = new CommandRelay(IncBet);
             DecreaseBetCommand = new CommandRelay(DecBet);
             MaxBetCommand = new CommandRelay(MaxBet);
             ConfirmBetCommand = new CommandRelay(Confirm);
         }
 
+        public int CurrentBet
+        {
+            get => _currentBet;
+            set
+            {
+                if (value <= _playerMoney && value >= 0)
+                    _currentBet = value;
+                OnPropertyChanged();
+
+            }
+        }
+        // No Setter because it's readonly
+        public int PlayerMoney
+        {
+            get => _playerMoney;
+        }
+
         private void IncBet()
         {
+            if (_currentBet + 10 <= _playerMoney)
+                CurrentBet += 10;
         }
 
         private void DecBet()
         {
-
+            if (_currentBet - 10 >= 0)
+                CurrentBet -= 10;
         }
 
         private void MaxBet()
         {
-
+            CurrentBet = _playerMoney;
         }
 
         private void Confirm()
         {
+
         }
     }
 }
