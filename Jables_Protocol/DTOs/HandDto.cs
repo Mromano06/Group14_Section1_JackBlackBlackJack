4ca125433d49cd3042b6bb8c1e8cffe300480826
SAM PELOT <sampelot93@gmail.com>
2026-03-13 15:56:04 -0400

    Added Card / Hand Dto stuff
    I added Card and Hand DTOs and their respective serializers and deserializers.

---
+++ Jables_Protocol/DTOs/HandDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharedModels.Models;
+
+namespace Jables_Protocol.DTOs
+{
+    internal class HandDto
+    {
+        public List<CardDto>? Cards { get; set; }   // the ? declares this as nullable
+    }
+}
