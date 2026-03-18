9f9f511701c023b0fcc7392e5e014044b5064636
b-arkell <broarkell2@gmail.com>
2026-03-18 18:34:31 -0400

    Created servercommandrelay

---
+++ Server/Commands/ServerCommandRelay.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Server.Commands
+{
+    public class ServerCommandRelay : ICommand
+    {
+
+        private readonly Action _execute;
+
+        public ServerCommandRelay(Action execute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter) => true;
+
+        public void Execute(object? parameter)
+        {
+            _execute();
+        }
+
+    }
+}
