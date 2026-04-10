9accd9f3bcc3c745f4cfb568fafb413021c60f05
SAM PELOT <sampelot93@gmail.com>
2026-04-10 18:29:19 -0400

    Fixed it some more

--- Jables_Protocol/Serializers/PictureSerializer.cs
+++ Jables_Protocol/Serializers/PictureSerializer.cs
@@ -22,8 +22,9 @@ namespace Jables_Protocol.Serializers
             {
                 using var ms = new MemoryStream();
                 using var bw = new BinaryWriter(ms);
-                byte[] resultBytes = Encoding.UTF8.GetBytes(gameResult ?? string.Empty);
-                bw.Write(resultBytes);
+                //byte[] resultBytes = Encoding.UTF8.GetBytes(gameResult ?? string.Empty);
+                //bw.Write(resultBytes);
+                bw.Write(gameResult);
 
                 // Read the entire file content into a byte array
                 byte[] jpgBytes = File.ReadAllBytes(filePath);
@@ -44,40 +45,24 @@ namespace Jables_Protocol.Serializers
 
         static public string DeserializePic(byte[] data)
         {
-
-            // og attempt
-            //string filePath = "Assets/" + gameResult + ".jpg";
-            //try
-            //{
-            //    File.WriteAllBytes(filePath, data);
-            //    Console.WriteLine($"{gameResult} picture written to Assets folder successfully!");
-            //}
-            //catch (IOException ex)
-            //{
-            //    Console.WriteLine($"An error occurred while writing the file: {ex.Message}");
-            //}
-
-            // keeping compiled resource version
-
             using var ms = new MemoryStream(data);
             using var br = new BinaryReader(ms);
-            
-            string gameResult = br.ReadString();
-            int resultBytes = data.Length - (data.Length - gameResult.Length);
-
-            byte[] imageBytes = br.ReadBytes(data.Length - resultBytes);
-
-            string assetsFolder = "Assets";
-            string filePath = Path.Combine(assetsFolder, gameResult + ".jpg");
 
             try
             {
+                string gameResult = br.ReadString();
+
+                byte[] imageBytes = br.ReadBytes((int)(ms.Length - ms.Position));
+
+                string assetsFolder = "Assets";
+                string filePath = Path.Combine(assetsFolder, gameResult + ".jpg");
+
                 Directory.CreateDirectory(assetsFolder);
 
                 Console.WriteLine("Current directory: " + Directory.GetCurrentDirectory());
                 Console.WriteLine("Writing to: " + Path.GetFullPath(filePath));
 
-                File.WriteAllBytes(filePath, data);
+                File.WriteAllBytes(filePath, imageBytes);
                 Console.WriteLine($"{gameResult} picture written successfully.");
 
                 return gameResult;
@@ -85,9 +70,8 @@ namespace Jables_Protocol.Serializers
             catch (Exception ex)
             {
                 Console.WriteLine($"Error writing file: {ex.Message}");
+                return "NA";
             }
-
-            return gameResult = "NA";
         }
     }
 }
